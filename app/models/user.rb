9614b7a2d1cac36288b5aa9cc0a44813a4097c3d
Todd Nestor <todd.nestor@gmail.com>
2016-10-06 09:48:52 -0700

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  validates :email, :session_token, :password_digest, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def find_by_credentials(email, password)
+    user = User.find_by_email(email)
+
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
---
+++ db/migrate/20161006163345_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string, :password_digest
+      t.string :session_token
+
+      t.timestamps
+    end
+    add_index :users, :session_token
+  end
+end
