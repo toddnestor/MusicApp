377b6e748cf35b829a4ebca6ccf249b17cea525e
Todd Nestor <todd.nestor@gmail.com>
2016-10-06 16:45:45 -0700

    now you must have an activated account to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by_credentials(session_params[:email], session_params[:password])
+    user.errors[:must_activate] = "your account by clicking the link we sent you in your e-mail before you can sign in." unless user.nil? || user.activated
 
-    if user
+    if user && user.errors.empty?
       add_message("Welcome back, #{user.email}!")
       login(user)
       redirect_to user_url(user)
     else
-      add_errors(user.errors.full_messages)
+      if user && !user.errors.empty?
+        add_errors(user.errors.full_messages)
+      else
+        add_error("We couldn't find a user with those credentials, please try again.")
+      end
       redirect_to new_session_url
     end
   end
